b176bf25f9212554cc8d3f68b286310b0ab500b0
daffa <daffaryasatya28@gmail.com>
2024-11-13 14:07:59 +0700

    ui 47.00

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,62 @@
+import React from 'react'
+import './Main.css'
+import { assets } from '../../assets/assets'
+
+const Main = () => {
+    return (
+        <div className='main'>
+
+            <div className='nav'>
+                <p>Dapp AI</p>
+                <img src={assets.user_icon} alt=''/>
+            </div>
+
+            <div className="main-container">
+                <div className="greet">
+                    <p><span>Hello, Sobat Dapp</span></p>
+                    <p>how can i help you today</p>
+                </div>
+
+
+                <div className="cards">
+                    <div className="card">
+                        <p>suggest beautiful places to see</p>
+                        <img src={assets.compass_icon} alt="" />
+                    </div>
+
+                    <div className="card">
+                        <p>suggest beautiful places to see</p>
+                        <img src={assets.bulb_icon} alt="" />
+                    </div>
+
+                    <div className="card">
+                        <p>suggest beautiful places to see</p>
+                        <img src={assets.message_icon} alt="" />
+                    </div>
+
+                    <div className="card">
+                        <p>suggest beautiful places to see</p>
+                        <img src={assets.code_icon} alt="" />
+                    </div>
+                </div>
+
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <input type="text" placeholder='Enter a prompt here' />
+                        <div>
+                            <img src={assets.gallery_icon} alt="" />
+                            <img src={assets.mic_icon} alt="" />
+                            <img src={assets.send_icon} alt="" />
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        gemini may display innacurate info
+                    </p>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Main
--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Sidebar.css'
 import '../../assets/assets'
 import { assets } from '../../assets/assets'
 
 const Sidebar = () => {
+
+  const [extended,setExtended] = useState(false)
+
   return (
     <div className='sidebar'>
 
       <div className="top">
-        <img className='menu' src={assets.menu_icon} alt="" />
+        <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" />
         <div className="new-chat">
             <img src={assets.plus_icon} alt="" />
-            <p>new chat</p>
+            {extended?<p>new chat</p>:null}
         </div>
-
+        
+        {/* dibungkus extended agar hilang jika kondisi */}
+        {extended?
         <div className="recent">
             <p className="recent-title">Recent</p>
             <div className="recent-entry">
@@ -21,22 +26,26 @@ const Sidebar = () => {
                 <p>What is react..</p>
             </div>
         </div>
+        :null}
+
+
+
       </div>
 
       <div className="bottom">
         <div className="bottom-item recent-entry">
             <img src={assets.question_icon} alt="" />
-            <p>Help</p>
+            {extended?<p>Help</p>:null}
         </div>
         
         <div className="bottom-item recent-entry">
             <img src={assets.history_icon} alt="" />
-            <p>Activity</p>
+            {extended?<p>Activity</p>:null}
         </div>
 
         <div className="bottom-item recent-entry">
             <img src={assets.setting_icon} alt="" />
-            <p>Settings</p>
+            {extended?<p>Settings</p>:null}
         </div>
       </div>
     </div>
