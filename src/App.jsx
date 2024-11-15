f07c92feb1f5da91239dcd19ff013bc55ad88539
daffa <daffaryasatya28@gmail.com>
2024-11-15 18:23:04 +0700

    analytics vercel

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
     <>
       <Sidebar/>
       <Main />
+      <Analytics />
     </>
   )
 }
