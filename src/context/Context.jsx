0a5314d7f4d501c8fb69fc49c1fa367893118c80
daffa <daffaryasatya28@gmail.com>
2024-11-15 12:41:46 +0700

    menambahkan ai yang tampil di ui ,loading, dan ketik

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,18 +1,74 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 import run from "../config/gemini";
+import runChat from "../config/gemini";
 
 export const Context = createContext();
 
 const ContextProvider = (props) => {
 
 
+    const [input,setInput] = useState('');
+    const [input2,setInput2] = useState('');
+    const [select,setSelect] = useState('');
+    const [recentPrompt,setRecentPrompt] = useState('');
+    const [prevPrompts,setPrevPrompts] = useState([]);
+    const [showResult,setShowResult] = useState(false);
+    const [loading,setLoading] = useState(false);
+    const [resultData,setResultData] = useState('');
+
     const  onSent = async (prompt) => {
-        await run(prompt)
+
+        setResultData('')
+
+        const delayPara = (index,nextWord) => {
+            setTimeout(function(){
+                setResultData(prev=>prev+nextWord);
+            },75*index)
+        }
+
+        setLoading(true)
+        setShowResult(true)
+        setRecentPrompt(input + select)
+        const response = await runChat(input + select)
+        let responseArray = response.split('**')
+        let newResponse;
+        for(let i=0 ; i<responseArray.length ; i++)
+        {
+            if(i===0 || i%2 !==1){
+                newResponse += responseArray[i]
+            }
+            else{
+                newResponse += "<b>"+responseArray[i]+"</b>"
+            }
+        }
+        let newResponse2 = newResponse.split('*').join("</br>")
+        let newResponseArray = newResponse2.split(' ')
+        for (let i = 0; i < newResponseArray.length; i++) {
+            const nextWord = newResponseArray[i];
+            delayPara(i,nextWord+' ')
+            
+        }
+        setLoading(false)
+        setInput('')
     }
 
-    onSent('buatkan planning umkm')
+    
 
     const contextValue = {
+        prevPrompts,
+        setPrevPrompts,
+        onSent,
+        setRecentPrompt,
+        recentPrompt,
+        showResult,
+        loading,
+        resultData,
+        input,
+        setInput,
+        input2,
+        setInput2,
+        select,
+        setSelect,
 
     }
 
