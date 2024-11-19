83b35eb625c33c04747644d3b46b8e3637fd4652
daffa <daffaryasatya28@gmail.com>
2024-11-19 08:34:49 +0700

    membenarkan kesalahan

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Sidebar from './components/Sidebar/Sidebar'
 import Main from './components/Main/Main'
 import { Analytics } from "@vercel/analytics/react"
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 const App = () => {
   return (
